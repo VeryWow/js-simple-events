ca13cad42d0daf25990db24074961485a38d7485
Karen <dev.kaskar2008@gmail.com>
2019-09-30 16:04:06 +0300

    Update build

--- cjs/index.js
+++ cjs/index.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var EventManagment = /** @class */ (function () {
     function EventManagment() {
-        this.eventHandlersMap = {};
+        this.eventHandlersMap = {
+            '*': new Map()
+        };
         this.isDebug = false;
         /// Aliases:
         this.fire = this.emit;
@@ -13,26 +15,32 @@ var EventManagment = /** @class */ (function () {
         ///
     }
     EventManagment.prototype.addEventHandler = function (eventName, callback, isOnce) {
-        var _this = this;
         if (isOnce === void 0) { isOnce = false; }
-        if (eventName instanceof RegExp) {
-            Object.keys(this.eventHandlersMap).forEach(function (el) {
-                if (el.match(eventName) && callback && !_this.eventHandlersMap[el].has(callback)) {
-                    _this.eventHandlersMap[el].set(callback, isOnce);
+        if (!this.eventHandlersMap[eventName]) {
+            this.eventHandlersMap[eventName] = new Map();
+        }
+        if (callback && !this.eventHandlersMap[eventName].has(callback)) {
+            this.eventHandlersMap[eventName].set(callback, isOnce);
+        }
+    };
+    EventManagment.prototype.callHandlers = function (eventName) {
+        var _this = this;
+        var args = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            args[_i - 1] = arguments[_i];
+        }
+        if (this.eventHandlersMap[eventName]) {
+            this.eventHandlersMap[eventName].forEach(function (isOnce, handler) {
+                handler && handler.apply(void 0, args.concat([{ eventName: eventName, isOnce: isOnce }]));
+                if (isOnce) {
+                    _this.off(eventName, handler);
                 }
             });
-        }
-        else {
-            if (!this.eventHandlersMap[eventName]) {
-                this.eventHandlersMap[eventName] = new Map();
-            }
-            if (callback && !this.eventHandlersMap[eventName].has(callback)) {
-                this.eventHandlersMap[eventName].set(callback, isOnce);
-            }
         }
     };
     EventManagment.prototype.setDebug = function (isDebug) {
         this.isDebug = isDebug;
+        return this;
     };
     EventManagment.prototype.on = function (eventName, callback) {
         this.addEventHandler(eventName, callback);
@@ -43,27 +51,15 @@ var EventManagment = /** @class */ (function () {
         return this;
     };
     EventManagment.prototype.off = function (eventName, callback) {
-        var _this = this;
-        if (eventName instanceof RegExp) {
-            Object.keys(this.eventHandlersMap).forEach(function (el) {
-                if (el.match(eventName) && callback && !_this.eventHandlersMap[el].has(callback)) {
-                    _this.eventHandlersMap[el].delete(callback);
-                }
-            });
+        if (!this.eventHandlersMap[eventName]) {
+            return this;
         }
-        else {
-            if (!this.eventHandlersMap[eventName]) {
-                return this;
-            }
-            if (callback && this.eventHandlersMap[eventName].has(callback)) {
-                this.eventHandlersMap[eventName].delete(callback);
-                return this;
-            }
+        if (callback && this.eventHandlersMap[eventName].has(callback)) {
+            this.eventHandlersMap[eventName].delete(callback);
         }
         return this;
     };
     EventManagment.prototype.emit = function (eventName) {
-        var _this = this;
         var args = [];
         for (var _i = 1; _i < arguments.length; _i++) {
             args[_i - 1] = arguments[_i];
@@ -71,14 +67,8 @@ var EventManagment = /** @class */ (function () {
         if (this.isDebug) {
             console.info("[" + this.constructor.name + "]: Fires " + eventName);
         }
-        if (this.eventHandlersMap[eventName]) {
-            this.eventHandlersMap[eventName].forEach(function (isOnce, handler) {
-                handler && handler.apply(void 0, args.concat([{ eventName: eventName, isOnce: isOnce }]));
-                if (isOnce) {
-                    _this.off(eventName, handler);
-                }
-            });
-        }
+        this.callHandlers.apply(this, ['*'].concat(args));
+        this.callHandlers.apply(this, [eventName].concat(args));
     };
     return EventManagment;
 }());
